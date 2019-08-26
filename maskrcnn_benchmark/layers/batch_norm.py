c4b6b62fe1071251537fe5b800cc738c2097e47f
Jianfeng Wang <jianfengwang@outlook.com>
2019-08-26 10:41:41 -0700

    u

--- maskrcnn_benchmark/layers/batch_norm.py
+++ maskrcnn_benchmark/layers/batch_norm.py
@@ -9,12 +9,18 @@ class FrozenBatchNorm2d(nn.Module):
     are fixed
     """
 
-    def __init__(self, n):
+    def __init__(self, n, eps=0):
+        # in pytorch batchnorm layer, eps is 1e-5. We assume the default init
+        # weight has merged 1e-5 and thus ignored this. Here, we set it as 0 by
+        # default to make it compatible with the original maskrcnn
+        # implementation, but it is noting that if the init weight comes from
+        # pytorch or torch vision, make sure eps is correctly set.
         super(FrozenBatchNorm2d, self).__init__()
         self.register_buffer("weight", torch.ones(n))
         self.register_buffer("bias", torch.zeros(n))
         self.register_buffer("running_mean", torch.zeros(n))
         self.register_buffer("running_var", torch.ones(n))
+        self.eps = eps
 
     def forward(self, x):
         # Cast all fixed parameters to half() if necessary
@@ -24,8 +30,11 @@ class FrozenBatchNorm2d(nn.Module):
             self.running_mean = self.running_mean.half()
             self.running_var = self.running_var.half()
 
-        scale = self.weight * self.running_var.rsqrt()
+        scale = self.weight * (self.running_var + self.eps).rsqrt()
         bias = self.bias - self.running_mean * scale
         scale = scale.reshape(1, -1, 1, 1)
         bias = bias.reshape(1, -1, 1, 1)
         return x * scale + bias
+
+    def extra_repr(self):
+        return '{}, eps={}'.format(len(self.weight), self.eps)
