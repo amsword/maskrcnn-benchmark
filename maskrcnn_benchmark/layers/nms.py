4937d28fbf72f6a9bb4dd3a12693085cdf0ee783
Jianfeng Wang <jianfengwang@outlook.com>
2020-02-08 16:05:59 -0800

    add hnms interface

--- maskrcnn_benchmark/layers/nms.py
+++ maskrcnn_benchmark/layers/nms.py
@@ -6,6 +6,7 @@ from apex import amp
 
 # Only valid with fp32 inputs - give AMP the hint
 nms = amp.float_function(_C.nms)
+hnms = _C.hnms
 
 # nms.__doc__ = """
 # This function performs Non-maximum suppresion"""
